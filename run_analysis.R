69028f976b21d3511f6ad419b69d9dfab521b275
crenneke <crenneke@gmx.de>
2016-11-20 22:45:42 +0100

    Additional commenting

--- run_analysis.R
+++ run_analysis.R
@@ -17,20 +17,22 @@ colnames(subj_train)<-c("Subject")
 xtest<-read.fwf("./UCI HAR Dataset/test/X_test.txt", widths=rep(16,561)) #Read x-test data
 colnames(xtest)<-features$V2 #Name columns based on features.txt
 ytest<-read.table("./UCI HAR Dataset/test/y_test.txt", header=FALSE) #Read y-test data
-colnames(ytest)<-c("Activity")
-subj_test<-read.table("./UCI HAR Dataset/test/subject_test.txt", header=FALSE)
-colnames(subj_test)<-c("Subject")
-activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt")
-colnames(activity_labels)<-c("Activity_Key", "Activity_Label")
+colnames(ytest)<-c("Activity") #Name activity column semantically
+subj_test<-read.table("./UCI HAR Dataset/test/subject_test.txt", header=FALSE) #Read subject data
+colnames(subj_test)<-c("Subject") #Name subject column semantically
+activity_labels<-read.table("./UCI HAR Dataset/activity_labels.txt") #Read activity labels
+colnames(activity_labels)<-c("Activity_Key", "Activity_Label") #Name activity columns for labelling semantically
 
 #Merge train and test files
 train<-cbind(ytrain,subj_train,xtrain) #combine y-train with x-train
 test<-cbind(ytest,subj_test,xtest) #combine y-test with x-test
 
-#Append train and test files, since the have the same structure
+#Append train and test files and label, since the have the same structure
 traintest<-rbind(train,test) #Append datasets since structurally the same
 tt_nodup<-traintest[,!duplicated(colnames(traintest))] #Deduplicate columns
-tt_nodup_label<-merge(tt_nodup,activity_labels,by.x="Activity", by.y="Activity_Key", all=TRUE)
+tt_nodup_label<-merge(tt_nodup,activity_labels,by.x="Activity", by.y="Activity_Key", all=TRUE) #Join activity labels based on activity keys
+
+#Select only Mean and Std Deviation variables and labels for subject and activity
 tt_final<-select(tt_nodup_label, grep("Mean|Std|Subject|Activity_Label", names(tt_nodup_label), ignore.case=TRUE)) #Only extract mean or std values
 tt_final_agg<-aggregate(tt_final, list(tt_final$Subject, tt_final$Activity_Label), FUN = "mean") #Aggregate by Subject and Activity with mean function
 tt_final_agg_clean<- select(rename(tt_final_agg, Subject_No=Group.1, Activity=Group.2), -c(Subject, Activity_Label)) #Rename and reduce columns
